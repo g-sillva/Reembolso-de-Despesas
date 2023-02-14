5fdf51b9820ce6cde253dbd79ba886150c663df1
g-sillva <silvagabriel.limars@gmail.com>
2023-02-14 16:10:26 -0300

    Implementada a entidade lançamento

--- server/src/main/java/com/fss/reembolso/lancamento/Lancamento.java
+++ server/src/main/java/com/fss/reembolso/lancamento/Lancamento.java
@@ -1,9 +1,16 @@
 package com.fss.reembolso.lancamento;
 
+import com.fss.reembolso.usuario.Usuario;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.bson.types.Binary;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
 
 @Document("lancamentos")
 @Data
@@ -13,5 +20,20 @@ public class Lancamento {
     @Id
     private String id;
 
+    @NotBlank(message = "O título não pode estar vazio")
     private String titulo;
+    private String descricao;
+    private Status status = Status.EM_RASCUNHO;
+
+    @CreatedDate
+    private LocalDate data;
+
+    @NotBlank(message = "A categoria não pode estar vazio")
+    private Categoria categoria;
+
+    @DBRef
+    private Usuario usuario;
+
+    @NotBlank(message = "A imagem não pode estar vazia")
+    private Binary img;
 }
