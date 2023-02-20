c2c13a56d222efa7e204662a836969edbe2f76dd
g-sillva <silvagabriel.limars@gmail.com>
2023-02-20 19:50:03 -0300

    Adicionado paginação nos lançamentos

--- server/src/main/java/com/fss/reembolso/lancamento/LancamentoService.java
+++ server/src/main/java/com/fss/reembolso/lancamento/LancamentoService.java
@@ -1,14 +1,15 @@
 package com.fss.reembolso.lancamento;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 
 public interface LancamentoService {
-    List<Lancamento> getTodosLancamentos(String titulo, String descricao, String status, String ano, String mes, String categoria);
-    List<Lancamento> getLancamentosPorUsuarioId(String usuario_id);
+    Page<Lancamento> getTodosLancamentos(String titulo, String descricao, String status, String ano, String mes, String categoria, Pageable pageable);
+    Page<Lancamento> getLancamentosPorUsuarioId(String usuario_id, Pageable pageable);
     Lancamento salvarLancamento(Lancamento lancamento, MultipartFile img) throws IOException;
     Lancamento getLancamentoPorId(String id);
     Lancamento patchLancamento(String id, Map<String, Object> field, MultipartFile img) throws IOException;
