bc1d717a1532e21a9654f6d16b07edbb6cac434a
g-sillva <silvagabriel.limars@gmail.com>
2023-02-22 15:48:13 -0300

    Adicionado componente header

---
+++ cliente/src/pages/tela_inicial/TelaInicial.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+
+import "./TelaInicial.css";
+
+function TelaInicial() {
+  return (
+    <section className='page-telainicial'>
+        <Header />
+    </section>
+  )
+}
+
+export default TelaInicial
