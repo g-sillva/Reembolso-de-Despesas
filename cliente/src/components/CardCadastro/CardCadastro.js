bcdbd621e2f872d9fc0b05265ac59c228a8cfb2e
Erick Malaguez Rowedder <erickmalaguezrowedder@gmail.com>
2023-02-28 17:07:32 -0300

    Atualização da tela de cadastro
    Agora, ao usuário clicar em "Cadastrar" irá ser enviado para a tela de envio de e-mail para validação de conta.

--- cliente/src/components/CardCadastro/CardCadastro.js
+++ cliente/src/components/CardCadastro/CardCadastro.js
@@ -1,42 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CardCadastro.css'
+import CardConfirmacaoCadastro from '../CardConfirmacaoCadastro/CardConfirmacaoCadastro'
 
-function CardCadastro({ aoClicarLinkCadastro }) {
+function CardCadastro({ aoClicarBotaoCadastro, aoClicarLinkCadastro }) {
+  const [isCardConfirmacaoCadastro, setCardConfirmacaoCadastro] = useState(false)
+
   return (
     <section className='card-cadastro-container'>
       <h1 className='card-cadastro-titulo'>Cadastrar</h1>
 
       <h2 className='card-cadastro-descricao'>Por favor, preencha as informações abaixo.</h2>
 
-      <form action='#' method='post' className='card-cadastro-form'>
+      {/* Remover depois o preventDefault do form */}
+
+      <form onSubmit={e => e.preventDefault()} action='#' method='post' className='card-cadastro-form'>
         <label for='name' className='card-cadastro-label'>Nome *</label>
-        <input type='text' id='name' className='card-cadastro-input' required></input>
+        <input type='text' id='nome-cadastro' className='card-cadastro-input'></input>
 
         <label for='email' className='card-cadastro-label'>E-mail *</label>
-        <input type='email' id='email' className='card-cadastro-input' required></input>
+        <input type='email' id='email-cadastro' className='card-cadastro-input'></input>
 
         <label for='email-confirm' className='card-cadastro-label'>Confirmar e-mail *</label>
-        <input type='email' id='email-confirm' className='card-cadastro-input' required></input>
+        <input type='email' id='email-cadastro-b' className='card-cadastro-input'></input>
 
         <label for='tel' className='card-cadastro-label'>Telefone *</label>
-        <input type='tel' id='card-cadastro-input-T' className='card-cadastro-input' required></input>
+        <input type='tel' id='card-cadastro-input-T' className='card-cadastro-input'></input>
 
         <label for='password' className='card-cadastro-label'>Senha *</label>
-        <input type='password' id='password' className='card-cadastro-input' required></input>
+        <input type='password' id='senha-cadastro' className='card-cadastro-input' onChange='confereSenha();'></input>
 
         <label for='password-confirm' className='card-cadastro-label'>Confirmar senha *</label>
-        <input type='password' id='password' className='card-cadastro-input' required></input>
+        <input type='password' id='senha-cadastro-b' className='card-cadastro-input' onChange='confereSenha();'></input>
 
-        <div className='card-cadastro-container-C'>
-          <input type='checkbox' id='card-cadastro-input-C' required></input><label for='checkbox-confim' id='card-cadastro-label-C'>Eu concordo com os <a className='card-cadastro-label-L' href='https://www.google.com.br/'>Temos e condições de uso</a>.</label>
+        <div className='card-cadastro-container-b'>
+          <input type='checkbox' id='card-cadastro-input-C'></input><label for='checkbox-confim' id='card-cadastro-label-C'>Eu concordo com os <a className='card-cadastro-label-L' href='https://www.google.com.br/'>Temos e condições de uso</a>.</label>
         </div>  
 
-        <input type='submit' value='CADASTRAR' id='card-cadastro-B-cadastrar'></input>
+        <input onClick={() => setCardConfirmacaoCadastro(!isCardConfirmacaoCadastro)} type='submit' value='CADASTRAR' id='card-cadastro-B-cadastrar'></input>
 
         <div className='card-cadastro-logar-conta-container'>
           <h2 className='card-cadastro-logar-conta'>Já possui cadastro? <p onClick={aoClicarLinkCadastro} className='card-cadastro-logar-conta-L'>Acesse sua conta</p></h2>
         </div>
       </form>
+
+      {isCardConfirmacaoCadastro && <CardConfirmacaoCadastro aoClicarBotaoCadastro={() => {
+        setCardConfirmacaoCadastro(true);
+      }}/>}
     </section>
   )
 }
