0dcabf56452c83d93adfe5ddd5ebefcec4c8e92d
g-sillva <silvagabriel.limars@gmail.com>
2023-03-04 23:30:13 -0300

    Implementada integração de registro de usuário com back-end e melhorada a tela de registro

--- cliente/src/components/cartao_confirmacao_cadastro/CardConfirmacaoCadastro.js
+++ cliente/src/components/cartao_confirmacao_cadastro/CardConfirmacaoCadastro.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './CardConfirmacaoCadastro.css'
-// import CardCadastro from '../CardCadastro/CardCadastro';
 
-function CardConfirmacaoCadastro() {
+function CardConfirmacaoCadastro({ email, aoClicarSair }) {
   return (
     <section className='card-confirmacao-cadastro-container'>
+      <i className="fa-solid fa-xmark" onClick={aoClicarSair}></i>
       <h1 className='card-confirmacao-cadastro-titulo'>Cadastrar</h1>
-      <div class='card-confirmacao-cadastro-bottom'>
+      <div className='card-confirmacao-cadastro-bottom'>
         <img src='/img/img-confirmacao-cadastro.png' alt='' className='card-confirmacao-cadastro-img'></img>
         <h2 className='card-confirmacao-cadastro-status'>QUASE PRONTO!</h2>
-        <h3 className='card-confirmacao-cadastro-subtitulo'>Clique no link que foi enviado para o e-mail <a href='https://www.google.com.br/' className='card-confirmacao-cadastro-subtitulo-L'>email@example.com</a> para ativar a sua conta!</h3>
+        <h3 className='card-confirmacao-cadastro-subtitulo'>Clique no link que foi enviado para o e-mail <span className='card-confirmacao-cadastro-subtitulo-L'>{email}</span> para ativar a sua conta!</h3>
       </div>
     </section>
   )
