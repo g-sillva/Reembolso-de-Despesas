060cee9ecff7a581c670c72cb0e2e8f2b2ecf70f
Erick Malaguez Rowedder <erickmalaguezrowedder@gmail.com>
2023-03-10 15:08:53 -0300

    Criação do js de adm login e cadastro.

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -5,9 +5,10 @@ import './App.css';
 import LoginCadastro from './pages/tela_login_cadastro/LoginCadastro';
 import TelaInicial from './pages/tela_inicial/TelaInicial';
 import TelaAdm from './pages/tela_adm/TelaAdm';
+import FormAdmLogin from './components/formulario_adm_login/FormAdmLogin';
+import FormAdmCadastro from './components/formulario_adm_cadastro/FormAdmCadastro';
 import Context from './Context';
 import { useState } from 'react';
-// import Loader from './components/loader/Loader';
 
 function App() {
   const [context, setContext] = useState(null);
@@ -20,10 +21,11 @@ function App() {
               <Route path='/LoginCadastro' element={<LoginCadastro />}/>
               <Route path= '' element={<TelaInicial />}/>
               <Route path='/TelaAdm' element={<TelaAdm />}/>
-              {/* <Route path='/Loader' element={<Loader />}/> */}
+              <Route path='/AdmLogin' element={<FormAdmLogin />}/>
+              <Route path='/AdmCadastro' element={<FormAdmCadastro />}/>
             </Routes>
-          </div>
-        </Context.Provider>
+        </div>
+      </Context.Provider>
     </div>  
    );
  }
---
+++ cliente/src/components/formulario_adm_login/FormAdmLogin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './FormAdmLogin.css'
+
+function FormAdmLogin() {
+  return (
+    <div>FormAdmLogin</div>
+  )
+}
+
+export default FormAdmLogin
